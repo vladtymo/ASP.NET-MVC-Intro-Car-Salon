c80eadc270c7baea86e51f7b69dca07fb4fe3748
vladtymo <tymo.vlad@gmail.com>
2021-12-15 20:20:47 +0200

    Add project files.

---
+++ AspNet_MVC_App/Controllers/CarController.cs
@@ -0,0 +1,39 @@
+﻿using AspNet_MVC_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AspNet_MVC_App.Controllers
+{
+    public class CarController : Controller
+    {
+        static List<Car> CarList = new List<Car>();
+
+        static CarController()
+        {
+            CarList.Add(new Car() { Model = "VW Passat B2", Color = "Red", ManufactureDate = new DateTime(1987, 12, 15) });
+            CarList.Add(new Car() { Model = "Nissan Juke", Color = "White", ManufactureDate = new DateTime(2010, 08, 5) });
+            CarList.Add(new Car() { Model = "VW Passat B3", Color = "Bordeux", ManufactureDate = new DateTime(1995, 9, 11) });
+        }
+
+        public IActionResult Index()
+        {
+            return View(CarList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Car newCar)
+        {
+            CarList.Add(newCar);
+            return View();
+        }
+    }
+}
---
+++ AspNet_MVC_App/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace AspNet_MVC_App.Models
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        [DisplayName("Manufacture Date")]
+        public DateTime ManufactureDate { get; set; }
+    }
+}
