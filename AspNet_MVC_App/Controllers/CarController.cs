9e4376d96a0d82564b8f54cbc53e7d2b74cdc5a0
vladtymo <tymo.vlad@gmail.com>
2021-12-17 20:36:41 +0200

    Server-side & Client-side Validations. Edit & Update Pages

--- AspNet_MVC_App/Controllers/CarController.cs
+++ AspNet_MVC_App/Controllers/CarController.cs
@@ -15,9 +15,9 @@ namespace AspNet_MVC_App.Controllers
 
         static CarController()
         {
-            CarList.Add(new Car() { Model = "VW Passat B2", Color = "Red", ManufactureDate = new DateTime(1987, 12, 15) });
-            CarList.Add(new Car() { Model = "Nissan Juke", Color = "White", ManufactureDate = new DateTime(2010, 08, 5) });
-            CarList.Add(new Car() { Model = "VW Passat B3", Color = "Bordeux", ManufactureDate = new DateTime(1995, 9, 11) });
+            CarList.Add(new Car() { Id=1, Model = "VW Passat B2", Color = "Red", ManufactureDate = new DateTime(1987, 12, 15) });
+            CarList.Add(new Car() { Id=2, Model = "Nissan Juke", Color = "White", ManufactureDate = new DateTime(2010, 08, 5) });
+            CarList.Add(new Car() { Id=3, Model = "VW Passat B3", Color = "Bordeux", ManufactureDate = new DateTime(1995, 9, 11) });
         }
 
         public IActionResult Index()
@@ -32,8 +32,39 @@ namespace AspNet_MVC_App.Controllers
         [HttpPost]
         public IActionResult Create(Car newCar)
         {
+            if (!ModelState.IsValid) return View();
+
             CarList.Add(newCar);
-            return View();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id <= 0) return NotFound();
+
+            CarList.Remove(CarList.First(c => c.Id == id));
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id <= 0) return NotFound();
+
+            var car = CarList.First(c => c.Id == id);
+            return View(car);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Car obj)
+        {
+            if (!ModelState.IsValid) return View();
+
+            var car = CarList.First(c => c.Id == obj.Id);
+            car.Model = obj.Model;
+            car.Color = obj.Color;
+            car.ManufactureDate = obj.ManufactureDate;
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- AspNet_MVC_App/Models/Car.cs
+++ AspNet_MVC_App/Models/Car.cs
@@ -9,7 +9,10 @@ namespace AspNet_MVC_App.Models
 {
     public class Car
     {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="You must enter a model")]
         public string Model { get; set; }
+        [Required(ErrorMessage = "You must enter a color")]
         public string Color { get; set; }
         [DisplayName("Manufacture Date")]
         public DateTime ManufactureDate { get; set; }
